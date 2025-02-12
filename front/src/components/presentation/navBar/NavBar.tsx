1c795537a592250f668ee0718309d9420b29b447
Theo Mousnier <mousniert.pro@gmail.com>
2025-02-13 00:03:45 +0100

    update navbar

--- front/src/components/presentation/navBar/NavBar.tsx
+++ front/src/components/presentation/navBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";  // Utilisation de React Router pour la navigation
 import "./NavBar.css";
 
 export default function NavBar() {
@@ -5,16 +6,18 @@ export default function NavBar() {
         <nav className="navbar">
             <div className="navbar-brand">
                 <span className="logo">TM</span>
-                <span className="brand-name">TeamManager</span>
+                <span className="brand-name">
+                     <Link to="/" className="navbar-link">TeamManager</Link> {/* Lien vers la page d'accueil */}
+                </span>
             </div>
             <ul className="navbar-links">
-                <li><a href="#features">Fonctionnalités</a></li>
-                <li><a href="#register-club">Inscrire un club</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">À propos</a></li>
+                <li><Link to="/features" className="navbar-link">Fonctionnalités</Link></li>  {/* Lien vers fonctionnalités */}
+                <li><Link to="/register-club" className="navbar-link">Inscrire un club</Link></li>  {/* Lien vers inscription club */}
+                <li><Link to="/contact" className="navbar-link">Contact</Link></li>  {/* Lien vers la page Contact */}
+                <li><Link to="/about" className="navbar-link">À propos</Link></li>  {/* Lien vers À propos */}
             </ul>
             <div className="navbar-actions">
-            <a href="#login" className="login">Se connecter</a>
+                <Link to="/login" className="login">Se connecter</Link>  {/* Lien vers la page de connexion */}
                 <a href="#join-team" className="register-btn">Rejoindre une équipe</a>
             </div>
         </nav>
