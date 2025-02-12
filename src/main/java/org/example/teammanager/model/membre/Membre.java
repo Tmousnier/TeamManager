fa8a499a1185b2cc15a19b620b03a81f0a5ed026
Theo Mousnier <mousniert.pro@gmail.com>
2025-02-13 00:03:27 +0100

    update back membre (model+repository)

--- src/main/java/org/example/teammanager/model/membre/Membre.java
+++ src/main/java/org/example/teammanager/model/membre/Membre.java
@@ -47,5 +47,88 @@ public class Membre {
             inverseJoinColumns = @JoinColumn(name = "idRoleMembre"))
     private List<RoleMembre> rolesMembre;
 
-    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public Date getDateDeNaissance() {
+        return dateDeNaissance;
+    }
+    public void setDateDeNaissance(Date dateDeNaissance) {
+        this.dateDeNaissance = dateDeNaissance;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Integer getNumeroTelephone() {
+        return numeroTelephone;
+    }
+    public void setNumeroTelephone(Integer numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Date getDateInscription() {
+        return dateInscription;
+    }
+    public void setDateInscription(Date dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+    public Equipe getEquipe() {
+        return equipe;
+    }
+    public void setEquipe(Equipe equipe) {
+        this.equipe = equipe;
+    }
+    public Club getClub() {
+        return club;
+    }
+    public void setClub(Club club) {
+        this.club = club;
+    }
+    public List<Joueur> getJoueurs() {
+        return joueurs;
+    }
+    public void setJoueurs(List<Joueur> joueurs) {
+        this.joueurs = joueurs;
+    }
+    public List<Message> getMessages() {
+        return messages;
+    }
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+    public List<RoleMembre> getRolesMembre() {
+        return rolesMembre;
+    }
+    public void setRolesMembre(List<RoleMembre> rolesMembre) {
+        this.rolesMembre = rolesMembre;
+    }
 }
