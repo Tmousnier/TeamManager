f265ced5464768e7a2f459e6ca0634e3f5652264
Theo Mousnier <mousniert.pro@gmail.com>
2025-02-13 00:02:54 +0100

    update main with routes

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import NavBar from "./components/presentation/navBar/NavBar";
-import Accueil from "./components/presentation/accueil/accueil.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importer React Router
+import NavBar from "./components/presentation/navBar/NavBar";  // Importer NavBar
+import Accueil from "./components/presentation/accueil/accueil.tsx";  // Page d'accueil
+import Login from "./components/presentation/connexion/connexion.tsx";  // Page de connexion
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <NavBar />
-        <Accueil />
+        <Router>
+            <NavBar /> {/* NavBar est en dehors des routes, donc visible sur toutes les pages */}
+            <Routes>
+                <Route path="/" element={<Accueil />} />  {/* Page d'accueil */}
+                <Route path="/login" element={<Login />} />  {/* Page de connexion */}
+                {/* Tu peux ajouter d'autres routes pour d'autres pages */}
+            </Routes>
+        </Router>
     </StrictMode>
 );
