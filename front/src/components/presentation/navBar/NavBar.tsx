ac177265b7113786286df7b15a11c298538c8946
Theo Mousnier <mousniert.pro@gmail.com>
2025-02-13 20:38:50 +0100

    update NavBar

--- front/src/components/presentation/navBar/NavBar.tsx
+++ front/src/components/presentation/navBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";  // Utilisation de React Router pour la navigation
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 export default function NavBar() {
@@ -7,18 +7,18 @@ export default function NavBar() {
             <div className="navbar-brand">
                 <span className="logo">TM</span>
                 <span className="brand-name">
-                     <Link to="/" className="navbar-link">TeamManager</Link> {/* Lien vers la page d'accueil */}
+                     <Link to="/" className="navbar-link">TeamManager</Link>
                 </span>
             </div>
             <ul className="navbar-links">
-                <li><Link to="/features" className="navbar-link">Fonctionnalités</Link></li>  {/* Lien vers fonctionnalités */}
-                <li><Link to="/register-club" className="navbar-link">Inscrire un club</Link></li>  {/* Lien vers inscription club */}
-                <li><Link to="/contact" className="navbar-link">Contact</Link></li>  {/* Lien vers la page Contact */}
-                <li><Link to="/about" className="navbar-link">À propos</Link></li>  {/* Lien vers À propos */}
+                <li><Link to="/fonctionnalites" className="navbar-link">Fonctionnalités</Link></li>
+                <li><Link to="/inscrireClub" className="navbar-link">Inscrire un club</Link></li>
+                <li><Link to="/contact" className="navbar-link">Contact</Link></li>
+                <li><Link to="/apropos" className="navbar-link">À propos</Link></li>
             </ul>
             <div className="navbar-actions">
-                <Link to="/login" className="login">Se connecter</Link>  {/* Lien vers la page de connexion */}
-                <a href="#join-team" className="register-btn">Rejoindre une équipe</a>
+                <Link to="/connexion" className="login">Se connecter</Link>
+                <Link to="/rejoindreEquipe" className="register-btn">Rejoindre une équipe</Link>
             </div>
         </nav>
     );
