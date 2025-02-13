52e6b2611c2a6b4585b83fd38de5ab6a6adf0a49
Theo Mousnier <mousniert.pro@gmail.com>
2025-02-13 20:40:02 +0100

    update router

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,17 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importer React Router
 import NavBar from "./components/presentation/navBar/NavBar";  // Importer NavBar
 import Accueil from "./components/presentation/accueil/accueil.tsx";  // Page d'accueil
-import Login from "./components/presentation/connexion/connexion.tsx";  // Page de connexion
-
+import Connexion from "./components/presentation/connexion/connexion.tsx";  // Page de connexion
+import Footer from "./components/presentation/footer/footer.tsx";
+import InscriptionClub from "./components/presentation/inscriptionClub/inscriptionClub.tsx";
+import RejoindreEquipe from "./components/presentation/rejoindreEquipe/rejoindreEquipe.tsx";
+import Contact from "./components/presentation/contact/contact.tsx";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
-            <NavBar /> {/* NavBar est en dehors des routes, donc visible sur toutes les pages */}
+            <NavBar />
             <Routes>
-                <Route path="/" element={<Accueil />} />  {/* Page d'accueil */}
-                <Route path="/login" element={<Login />} />  {/* Page de connexion */}
-                {/* Tu peux ajouter d'autres routes pour d'autres pages */}
+                <Route path="/" element={<Accueil/>} />
+                <Route path="/connexion" element={<Connexion/>} />
+                <Route path="/inscrireClub" element={<InscriptionClub/>} />
+                <Route path="/rejoindreEquipe" element={<RejoindreEquipe/>} />
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
+            <Footer/>
         </Router>
     </StrictMode>
 );
