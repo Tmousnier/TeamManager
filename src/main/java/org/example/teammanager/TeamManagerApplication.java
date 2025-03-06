e7f014f88a450e9bfa73f61c69c8c34e6e151278
Theo Mousnier <mousniert.pro@gmail.com>
2025-03-06 21:07:52 +0100

    Back update TeamManagerApplication

--- src/main/java/org/example/teammanager/TeamManagerApplication.java
+++ src/main/java/org/example/teammanager/TeamManagerApplication.java
@@ -2,8 +2,10 @@ package org.example.teammanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.example.teammanager", "org.example.teammanager.service", "org.example.teammanager.config"}) // Inclure les packages pertinents
 public class TeamManagerApplication {
 
     public static void main(String[] args) {
