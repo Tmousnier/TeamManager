3b84041041781df7f322be9c9a991dab92fd1be5
Theo Mousnier <mousniert.pro@gmail.com>
2025-03-06 21:08:48 +0100

    Back create DTO message erreur

---
+++ src/main/java/org/example/teammanager/dto/MessageErreurResponse.java
@@ -0,0 +1,9 @@
+package org.example.teammanager.dto;
+
+public class MessageErreurResponse {
+    public String message;
+
+    public MessageErreurResponse(String message) {
+        this.message = message;
+    }
+}
