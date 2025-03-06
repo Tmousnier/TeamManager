f19ab48116ffc3e7e5c4620ef0c86e7436ba9f8b
Theo Mousnier <mousniert.pro@gmail.com>
2025-03-06 21:05:29 +0100

    Back update webconfig

--- src/main/java/org/example/teammanager/config/WebConfig.java
+++ src/main/java/org/example/teammanager/config/WebConfig.java
@@ -1,8 +1,8 @@
 package org.example.teammanager.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080") // Your React app's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
-                .allowedHeaders("*"); // Allowed headers (or specify)
+                .allowedOrigins("http://localhost:5173") // URL de ton frontend en développement
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
     }
 }
