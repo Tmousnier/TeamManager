f1c2d8e551acb943750b571600fcf8ae9b14bfa6
Theo Mousnier <mousniert.pro@gmail.com>
2025-05-07 14:22:45 +0200

    Back-end update Model

--- src/main/java/org/example/teammanager/dto/MessageErreurResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.teammanager.dto;
-
-public class MessageErreurResponse {
-    public String message;
-
-    public MessageErreurResponse(String message) {
-        this.message = message;
-    }
-}
---
+++ src/main/java/org/example/teammanager/dto/contact/MessageErreurResponse.java
@@ -0,0 +1,10 @@
+package org.example.teammanager.dto.contact;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageErreurResponse {
+    private String message;
+}
